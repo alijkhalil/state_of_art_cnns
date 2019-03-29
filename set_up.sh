efc7928a576401c3d4f370470a01412c0437edb9
Ubuntu <ubuntu@ip-172-31-55-118.ec2.internal>
2019-03-29 05:18:33 +0000

    Changed set up script to longer use git hash

--- set_up.sh
+++ set_up.sh
@@ -2,27 +2,25 @@
 
 # Set up variables and local directory
 UTIL_REPO_NAME="dl_utilities"
-UTIL_REPO_GIT_HASH="6ed7c4c8e089e28a15c32e1b8dfb071d16e2c6c2"
 
 cd `dirname $0`
-
 
 # Get necessary repo's
 echo -n "Getting other repositories needed for the project...  "
 
-if [ ! -d $UTIL_REPO_NAME ]; then
-    git clone git@github.com:alijkhalil/"$UTIL_REPO_NAME".git > /dev/null 2>&1
-    
+if [ ! -d ../$UTIL_REPO_NAME ]; then
+    if [ -d $UTIL_REPO_NAME ]; then
+        rm -rf $UTIL_REPO_NAME
+    fi
+
+    git clone https://github.com/alijkhalil/$UTIL_REPO_NAME.git > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "Download error!"
+        echo "Download error!" >&2
         exit 1
     fi
+
+    mv $UTIL_REPO_NAME ../
 fi
-
-cd $UTIL_REPO_NAME
-git checkout $UTIL_REPO_GIT_HASH > /dev/null 2>&1
-cd - > /dev/null 2>&1
-
 
 # Print success and exit
 echo "Done!"
